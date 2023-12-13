2858cf5caad086fe250cd7a3f0a002ab63cab980
Pedro Silveira <bluelement1235@gmail.com>
2023-12-13 17:51:50 -0500

    renamed the database

--- FinalProjectWPFResumeBuilder/CategoryDBHandler.cs
+++ FinalProjectWPFResumeBuilder/CategoryDBHandler.cs
@@ -11,7 +11,7 @@ namespace FinalProjectResumeMaker
 {
     class CategoryDBHandler
     {
-        static readonly string conString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+        static readonly string conString = ConfigurationManager.ConnectionStrings["CategoryDatabase"].ConnectionString;
         static readonly CategoryDBHandler instance = new CategoryDBHandler();
 
 
